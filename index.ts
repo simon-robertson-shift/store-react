9375b5ea92536cdec4f3b203cc54d97269b186c4
Simon Robertson <87704046+simon-robertson-shift@users.noreply.github.com>
2023-07-07 18:13:39 +0100

    Initial version

---
+++ index.ts
@@ -0,0 +1,31 @@
+import type {
+    Reactive,
+    ReactiveActions,
+    ReactiveState,
+    ReactiveStateReceiver,
+    ReactiveStateSelector
+} from '@simon-robertson-online/reactive/types'
+
+import { useEffect, useState } from 'react'
+
+/** */
+export const useReactiveState = <S extends ReactiveState, T>(
+    reactive: Reactive<ReactiveActions, S>,
+    selector: ReactiveStateSelector<S, T>
+): T => {
+    const [value, setValue] = useState(() => selector(reactive.getState()))
+
+    useEffect(() => {
+        const receiver: ReactiveStateReceiver<S> = (state) => {
+            setValue(selector(state))
+        }
+
+        reactive.connect(receiver)
+
+        return () => {
+            reactive.disconnect(receiver)
+        }
+    }, [])
+
+    return value
+}
